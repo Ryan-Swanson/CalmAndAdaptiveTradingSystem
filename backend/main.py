af9bfce7940d54bfdda1f7158d92679a8efe05de
ryan-swanson <rswanson95@gmail.com>
2025-08-13 15:06:13 -0700

    2-6 Configure CORS in backend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="CalmAndAdaptiveTradingSystem API")
+
+# CORS - to be configured with deployed frontend domain in env NEXT_PUBLIC_FRONTEND_URL
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=["*"],
+	allow_credentials=True,
+	allow_methods=["*"],
+	allow_headers=["*"],
+)
 
 
 @app.get("/")
